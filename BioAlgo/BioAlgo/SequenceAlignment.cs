d8eed5574fe6fc60ef9d96b80cc7f3b0b7c86e3d
Unknown <kamila.herkova@gmail.com>
2018-03-18 17:07:06 +0100

    Initial commit of sources.

---
+++ BioAlgo/BioAlgo/SequenceAlignment.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace BioAlgo
+{
+    public class SequenceAlignment
+    {
+        static int[,] editDists;
+        static string[] lastSeqs = new string[2];
+
+        // Count the number of positions in which two strings differ
+        // Return -1 when two strings have different length
+        static public int CountHammingDistance (string a, string b)
+        {
+            int dist = 0;
+            if (a.Length != b.Length)
+                return -1;
+            
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] != b[i])
+                    ++dist;
+            }
+            return dist;
+        }
+
+        // Count the minimum number of operations needed to turn string a into string b.
+        // By edit operations we understand inserting a character into a sequence, deletion of a character from a sequence and a character replacement. 
+        // We assume that both strings are not empty or null.
+        static public int CountEditDistance (string a, string b)
+        {
+            int firstLen = a.Length + 1;
+            int secondLen = b.Length + 1;
+            lastSeqs = new string[]{ a, b };
+
+            editDists = new int[firstLen, secondLen];
+
+            for (int i = 0; i < firstLen; i++)
+                editDists[i, 0] = i;
+            for (int j = 0; j < secondLen; j++)
+                editDists[0, j] = j;
+
+            int delta(int i, int j) => a[i - 1] == b[j - 1] ? 0 : 1;           
+
+            int findMinimum(int i, int j)
+            {
+                int[] vals = new int[] {
+                        editDists[i - 1, j] + 1,
+                        editDists[i, j - 1] + 1,
+                        editDists[i - 1, j - 1] + delta(i, j)};
+                int min = Math.Min(vals[0], vals[1]);
+                min = Math.Min(min, vals[2]);
+                return min;
+            }
+
+            for (int i = 1; i < firstLen; i++)
+            {
+                for (int j = 1; j < secondLen; j++)
+                {
+                    editDists[i, j] = findMinimum(i, j);
+                }
+            }
+            return editDists[a.Length - 1, b.Length - 1];
+        }
+
+        // Returns all alignments of two strings using matrix of edit distances
+        public void FindAllAlignmentsUsingEditDistance(string a, string b)
+        {
+            if (!(a.Equals(lastSeqs[0]) && b.Equals(lastSeqs[1])))
+                CountEditDistance(a, b);
+
+            int i = editDists.GetLength(0), j = editDists.GetLength(1);
+            while (i != 0 && j != 0)
+            {
+
+            }
+        }
+    }
+}
---
+++ BioAlgo/BioAlgoTests/BioAlgoTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BioAlgo;
+
+namespace BioAlgoTests
+{
+    [TestClass]
+    public class BioAlgoTests
+    {
+        [TestMethod]
+        public void CountHammingDistance_DifferentLength_ReturnsMinus1()
+        {
+            string a = "blabla";
+            string b = "patla";
+            int dist = SequenceAlignment.CountHammingDistance(a, b);
+            Assert.AreEqual(-1, dist);
+        }
+
+        [TestMethod]
+        public void CountHammingDistance_SameLength()
+        {
+            string[] seqs = new string[] { "abba", "baba", "cdef" };
+            int[] expected = new int[] { 0, 2, 4 };
+            int dist;
+            for (int i = 0; i < seqs.Length; i++)
+            {
+                dist = SequenceAlignment.CountHammingDistance(seqs[0], seqs[i]);
+                Assert.AreEqual(expected[i], dist);
+            }
+        }
+
+        [TestMethod]
+        public void CountEditDistance_SameString_ReturnsZero()
+        {
+            string a = "red";
+            int dist = SequenceAlignment.CountEditDistance(a, a);
+            Assert.AreEqual(0, dist);
+        }
+
+        [TestMethod]
+        public void CountEditDistance_OneReplacement_ReturnsOne()
+        {
+            string a = "doll";
+            string b = "poll";
+            int dist = SequenceAlignment.CountEditDistance(a, b);
+            Assert.AreEqual(1, dist);
+        }
+
+        [TestMethod]
+        public void CountEdistDistance_OneDeletion_ReturnsOne()
+        {
+            string a = "anna";
+            string b = "ann";
+            int dist = SequenceAlignment.CountEditDistance(a, b);
+            Assert.AreEqual(1, dist);
+        }
+
+        [TestMethod]
+        public void CountEditDistance_OneInsertion_ReturnsOne()
+        {
+            string a = "les";
+            string b = "ples";
+            int dist = SequenceAlignment.CountEditDistance(a, b);
+            Assert.AreEqual(1, dist);
+        }
+    }
+}
